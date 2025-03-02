f737f4ba46cf960c8f8e1ac5a7e77789c60a8bc6
Paolo Pietrelli <pablos3339@gmail.com>
2025-03-02 06:38:48 +0100

    circolar queue test and class

--- tdd-exercise/src/main/java/tdd/CircularQueueSimple.java
+++ tdd-exercise/src/main/java/tdd/CircularQueueSimple.java
@@ -1,4 +1,42 @@
 package tdd;
 
+import java.util.LinkedList;
+
 public class CircularQueueSimple implements CircularQueue{
+    private final int capacity;
+    private final LinkedList<Integer> queue = new LinkedList<>();
+    private int oldestIndex;
+
+    public CircularQueueSimple(int capacity) {
+        this.capacity = capacity;
+        oldestIndex = 0;
+    }
+
+
+    @Override
+    public void push(int value) {
+        if (queue.size() == capacity) {
+            queue.remove(oldestIndex);
+            queue.add(oldestIndex, value);
+            if (oldestIndex >= capacity - 1) {
+                oldestIndex = 0;
+            } else {
+                oldestIndex ++;
+            }
+        } else {
+            queue.add(value);
+        }
+    }
+
+    @Override
+    public int pop() {
+        if (queue.isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        } else {
+            if(oldestIndex >= queue.size()) {
+                oldestIndex = 0;
+            }
+            return this.queue.remove(oldestIndex);
+        }
+    }
 }
--- tdd-exercise/src/test/java/tdd/CircularListTest.java
+++ tdd-exercise/src/test/java/tdd/CircularListTest.java
@@ -1,16 +1,34 @@
 package tdd;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+    CircularQueueSimple queue;
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void initializingTest(){
+        queue = new CircularQueueSimple(4);
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        assertEquals(1, queue.pop());
+        assertEquals(2, queue.pop());
+        queue.push(5);
+        queue.push(6);
+        queue.push(7);
+        assertEquals(4, queue.pop());
+        assertEquals(5, queue.pop());
+        assertEquals(6, queue.pop());
+        assertEquals(7, queue.pop());
+        Exception exception = assertThrows( IllegalStateException.class, ()->{
+            queue.pop();
+        });
+        assertEquals("Queue is empty", exception.getMessage());
     }
 }
